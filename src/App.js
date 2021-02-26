6ffa545d3ead52d5f0956a5305831e052f798ed8
Chris <chris@chriss-mac-mini.myfiosgateway.com>
2021-02-25 20:50:57 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Chris Lazntier</h1>
+          <marquee>Coming Soon!!!</marquee>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
