eb15f36f255fd1310ea52af353c3ce33090e5f49
Chris <chris@chriss-mac-mini.myfiosgateway.com>
2021-02-25 20:56:56 -0500

    new

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>Chris Lazntier</h1>
-          <marquee>Coming Soon!!!</marquee>
         </header>
       </div>
     </>
