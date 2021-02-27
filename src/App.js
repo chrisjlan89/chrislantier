5e1c6863e8c8f0b35aadb69e2bccaa2b7a1d84a1
Chris <chris@chriss-mac-mini.myfiosgateway.com>
2021-02-26 20:10:26 -0500

    yee

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import me from "../src/assets/images/Screen Shot 2021-02-26 at 7.56.24 PM.png";
 
 function App() {
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <h1>Chris Lazntier</h1>
+          <h1>Chris Lantier</h1>
+          <h3 className="animated"> Coming Soon!</h3>
+          <img className="img" alt="That's me Chris Lazntier" src={me} />
         </header>
       </div>
     </>
